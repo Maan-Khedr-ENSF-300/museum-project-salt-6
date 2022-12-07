acbc8e1353c670fb46cc269af73aa9801e9689d8
joshua girma debele <joshdebele8@gmail.com>
2022-12-06 21:32:46 -0700

    Collections Table

---
+++ SQL Scripts/sql database.sql
@@ -0,0 +1,17 @@
+Drop DATABASE if Exists Art_Museum
+Create Database Art_Museum;
+Use Art_Museum;
+
+
+Create Table Collections
+(
+    Name   varchar(20)      NOT NULL,
+    Address varchar(25)     NOT NULL,
+    Type  varchar(20)       NOT NULL,
+    Description varchar(50) NOT NULL,
+    Phone       varchar(12)     NOT NULL,
+    Contact__Person     varchar(20)    NOT NULL,
+
+    PRIMARY KEY (Name)
+);
+
